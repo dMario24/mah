194f83a8dd06def269dd3136614a2117b33c329d
dMario24 <data.mario24@gmail.com>
2024-07-25 17:20:28 +0900

    type

--- src/mah/cli.py
+++ src/mah/cli.py
@@ -14,20 +14,26 @@ def cmd():
                     epilog='Text at the bottom of help')
 
     parser.add_argument('-s', '--scount') 
-    parser.add_argument('-t', '--top')
+    parser.add_argument('-t', '--top', type=int)
     parser.add_argument('-d', '--dt' )
+    parser.add_argument('-p', '--pretty', action='store_true')
 
     args = parser.parse_args()
-    print(args.scount, args.top, args.dt)
+    #print(args.scount, args.top, args.dt)
 
     if args.scount: 
-        print(f"-s => {args.scount}")
+        # print(f"-s => {args.scount}")
         # TODO 명령어 카운트
+        r = count(args.scount)
+        print (f"{args.scount} 사용 횟수는 {r}회 입니다.")
     elif args.top:
-        print(f"-t => {args.top}")
+        # print(f"-t => {args.top}")
         if args.dt:
-            print(f"-d => {args.dt}")
+            # print(f"-d => {args.dt}")
             # TODO 특정 날짜의 명령어 TOP N
+            r = top(args.top, args.dt)
+            #r = top(int("10"), "2024-07-17")
+            print(r)
         else:
             parser.error("-t 옵션은 -d 옵션과 함께 사용하시오!")
     else:
